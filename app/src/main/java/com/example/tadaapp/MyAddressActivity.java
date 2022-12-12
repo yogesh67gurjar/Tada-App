318ecdc43bb9157698c91be70e89e6a3e1908586
yogesh gurjar <yogesh67gurjar@gmail.com>
2022-12-12 12:18:23 +0530

    items added by yogesh

---
+++ app/src/main/java/com/example/tadaapp/MyAddressActivity.java
@@ -0,0 +1,15 @@
+package com.example.tadaapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_address);
+
+    }
+}
